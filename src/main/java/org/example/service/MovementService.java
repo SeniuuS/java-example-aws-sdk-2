363cb517b8513cdc6769dcb4d2b4929b0d381d93
SeniuuS <seniuu.s1@gmail.com>
2022-10-19 16:23:51 +0200

    Factorisation of movement detection method

--- src/main/java/org/example/service/MovementService.java
+++ src/main/java/org/example/service/MovementService.java
@@ -2,6 +2,7 @@ package org.example.service;
 
 import software.amazon.awssdk.services.rekognition.model.FaceDetection;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MovementService {
@@ -11,7 +12,7 @@ public class MovementService {
     private static final float MOVEMENT_THRESHOLD = 2;
 
     private enum MovementType {
-        YAW_RIGHT, YAW_LEFT, PITCH_UP, PITCH_DOWN
+        YAW_RIGHT, YAW_LEFT, PITCH_UP, PITCH_DOWN, YAW, PITCH
     }
 
     private class Movement {
@@ -50,6 +51,26 @@ public class MovementService {
         }
 
         @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            Movement movement = (Movement) o;
+
+            if (startTimestamp != movement.startTimestamp) return false;
+            if (endTimestamp != movement.endTimestamp) return false;
+            return movementType == movement.movementType;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = movementType != null ? movementType.hashCode() : 0;
+            result = 31 * result + startTimestamp;
+            result = 31 * result + endTimestamp;
+            return result;
+        }
+
+        @Override
         public String toString() {
             return "Movement{" +
                     "movementType=" + movementType +
@@ -69,15 +90,16 @@ public class MovementService {
                 System.out.println("There is a smile : " + smile);
                 System.out.println(face.face().pose());
         });
-        Movement yaw = hasMovedYaw(faces);
+        List<Movement> movements = new ArrayList<>();
+        Movement yaw = getMovementList(faces, MovementType.YAW);
         if(yaw != null)
             System.out.println(yaw.toString());
-        Movement pitch = hasMovedPitch(faces);
+        Movement pitch = getMovementList(faces, MovementType.PITCH);
         if(pitch != null)
             System.out.println(pitch.toString());
     }
 
-    private Movement hasMovedYaw(List<FaceDetection> faces) {
+    private Movement getMovementList(List<FaceDetection> faces, MovementType movementType) {
         int startTimestamp = 0;
         int endTimestamp = 0;
         float beforeValue = 0;
@@ -86,58 +108,31 @@ public class MovementService {
         boolean thresholdReached = false;
         boolean isNegative = false;
         for(FaceDetection face : faces) {
-            float rawYaw = face.face().pose().yaw();
-            float yaw = Math.abs(rawYaw);
-            if(beforeValue < yaw && Math.abs(beforeValue - yaw) > MOVEMENT_THRESHOLD) {
-                beforeValue = yaw;
+            float rawMovementValue = movementType == MovementType.YAW ? face.face().pose().yaw() : face.face().pose().pitch();
+            float movementValue = Math.abs(rawMovementValue);
+            if(beforeValue < movementValue && Math.abs(beforeValue - movementValue) > MOVEMENT_THRESHOLD) {
+                beforeValue = movementValue;
                 nbInc++;
                 if(startTimestamp == 0)
                     startTimestamp = face.timestamp().intValue();
             }
-            else if(beforeValue > yaw && Math.abs(yaw - beforeValue) > MOVEMENT_THRESHOLD) {
-                beforeValue = yaw;
+            else if(beforeValue > movementValue && Math.abs(movementValue - beforeValue) > MOVEMENT_THRESHOLD) {
+                beforeValue = movementValue;
                 nbDown++;
                 if(endTimestamp == 0)
                     endTimestamp = face.timestamp().intValue();
             }
-            if(yaw > YAW_THRESHOLD) {
+            if(movementValue > (movementType == MovementType.YAW ? YAW_THRESHOLD : PITCH_THRESHOLD)) {
                 thresholdReached = true;
-                isNegative = rawYaw < 0;
+                isNegative = rawMovementValue < 0;
             }
         }
-        return thresholdReached && nbInc >= 2 && nbDown >= 2 ? new Movement(isNegative ? MovementType.YAW_LEFT : MovementType.YAW_RIGHT, startTimestamp, endTimestamp) : null;
+        return thresholdReached && nbInc >= 2 && nbDown >= 2 ?
+                new Movement(isNegative ?
+                                (movementType == MovementType.YAW ? MovementType.YAW_LEFT : MovementType.PITCH_DOWN) :
+                                (movementType == MovementType.YAW ? MovementType.YAW_RIGHT : MovementType.PITCH_UP),
+                        startTimestamp,
+                        endTimestamp)
+                : null;
     }
-
-
-    private Movement hasMovedPitch(List<FaceDetection> faces) {
-        int startTimestamp = 0;
-        int endTimestamp = 0;
-        float beforeValue = 0;
-        int nbInc = 0;
-        int nbDown = 0;
-        boolean thresholdReached = false;
-        boolean isNegative = false;
-        for(FaceDetection face : faces) {
-            float rawPitch = face.face().pose().pitch();
-            float pitch = Math.abs(rawPitch);
-            if(beforeValue < pitch && Math.abs(beforeValue - pitch) > MOVEMENT_THRESHOLD) {
-                beforeValue = pitch;
-                nbInc++;
-                if(startTimestamp == 0)
-                    startTimestamp = face.timestamp().intValue();
-            }
-            else if(beforeValue > pitch && Math.abs(pitch - beforeValue) > MOVEMENT_THRESHOLD) {
-                beforeValue = pitch;
-                nbDown++;
-                if(endTimestamp == 0)
-                    endTimestamp = face.timestamp().intValue();
-            }
-            if(pitch > PITCH_THRESHOLD) {
-                thresholdReached = true;
-                isNegative = rawPitch < 0;
-            }
-        }
-        return thresholdReached && nbInc >= 2 && nbDown >= 2 ? new Movement(isNegative ? MovementType.PITCH_DOWN : MovementType.PITCH_UP, startTimestamp, endTimestamp) : null;
-    }
-
 }
