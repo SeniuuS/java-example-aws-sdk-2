54be1c4d47bee6bae426db57e60dc87211685c77
SeniuuS <seniuu.s1@gmail.com>
2022-10-19 17:48:21 +0200

    Completed movement service detecting all movements and retrieving a list of movements

--- src/main/java/org/example/service/MovementService.java
+++ src/main/java/org/example/service/MovementService.java
@@ -3,13 +3,14 @@ package org.example.service;
 import software.amazon.awssdk.services.rekognition.model.FaceDetection;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class MovementService {
 
-    private static final float YAW_THRESHOLD = 50;
-    private static final float PITCH_THRESHOLD = 50;
-    private static final float MOVEMENT_THRESHOLD = 2;
+    private static final float YAW_THRESHOLD = 55;
+    private static final float PITCH_THRESHOLD = 40;
+    private static final float MOVEMENT_THRESHOLD = 5;
 
     private enum MovementType {
         YAW_RIGHT, YAW_LEFT, PITCH_UP, PITCH_DOWN, YAW, PITCH
@@ -82,24 +83,24 @@ public class MovementService {
 
     public void detectMovement(List<FaceDetection> faces) {
         faces.forEach((face) -> {
-                String age = face.face().ageRange().toString();
-                String smile = face.face().smile().toString();
-                System.out.println("The detected face is estimated to be"
-                        + age + " years old.");
-                System.out.println("There is a beard : " + face.face().beard().toString());
-                System.out.println("There is a smile : " + smile);
-                System.out.println(face.face().pose());
+            String age = face.face().ageRange().toString();
+            String smile = face.face().smile().toString();
+            System.out.println("The detected face is estimated to be"
+                    + age + " years old.");
+            System.out.println("There is a beard : " + face.face().beard().toString());
+            System.out.println("There is a smile : " + smile);
+            System.out.println(face.face().pose());
+            System.out.println(face.timestamp().intValue());
         });
         List<Movement> movements = new ArrayList<>();
-        Movement yaw = getMovementList(faces, MovementType.YAW);
-        if(yaw != null)
-            System.out.println(yaw.toString());
-        Movement pitch = getMovementList(faces, MovementType.PITCH);
-        if(pitch != null)
-            System.out.println(pitch.toString());
+        movements.addAll(getMovementList(faces, MovementType.YAW));
+        movements.addAll(getMovementList(faces, MovementType.PITCH));
+        movements.sort(Comparator.comparingInt(Movement::getStartTimestamp));
+        movements.forEach((movement -> System.out.println(movement.toString())));
     }
 
-    private Movement getMovementList(List<FaceDetection> faces, MovementType movementType) {
+    private List<Movement> getMovementList(List<FaceDetection> faces, MovementType movementType) {
+        List<Movement> movements = new ArrayList<>();
         int startTimestamp = 0;
         int endTimestamp = 0;
         float beforeValue = 0;
@@ -108,6 +109,7 @@ public class MovementService {
         boolean thresholdReached = false;
         boolean isNegative = false;
         for(FaceDetection face : faces) {
+            boolean reset = false;
             float rawMovementValue = movementType == MovementType.YAW ? face.face().pose().yaw() : face.face().pose().pitch();
             float movementValue = Math.abs(rawMovementValue);
             if(beforeValue < movementValue && Math.abs(beforeValue - movementValue) > MOVEMENT_THRESHOLD) {
@@ -116,7 +118,7 @@ public class MovementService {
                 if(startTimestamp == 0)
                     startTimestamp = face.timestamp().intValue();
             }
-            else if(beforeValue > movementValue && Math.abs(movementValue - beforeValue) > MOVEMENT_THRESHOLD) {
+            else if(nbInc > 0 && beforeValue > movementValue && Math.abs(movementValue - beforeValue) > MOVEMENT_THRESHOLD) {
                 beforeValue = movementValue;
                 nbDown++;
                 if(endTimestamp == 0)
@@ -126,13 +128,25 @@ public class MovementService {
                 thresholdReached = true;
                 isNegative = rawMovementValue < 0;
             }
+            if(nbInc >= 1 && nbDown >= 1){
+                if(thresholdReached) {
+                    movements.add(new Movement(isNegative ?
+                            (movementType == MovementType.YAW ? MovementType.YAW_LEFT : MovementType.PITCH_DOWN) :
+                            (movementType == MovementType.YAW ? MovementType.YAW_RIGHT : MovementType.PITCH_UP),
+                            startTimestamp,
+                            endTimestamp));
+                }
+                reset = true;
+            }
+            if(reset) {
+                thresholdReached = false;
+                isNegative = false;
+                nbInc = 0;
+                nbDown = 0;
+                startTimestamp = 0;
+                endTimestamp = 0;
+            }
         }
-        return thresholdReached && nbInc >= 2 && nbDown >= 2 ?
-                new Movement(isNegative ?
-                                (movementType == MovementType.YAW ? MovementType.YAW_LEFT : MovementType.PITCH_DOWN) :
-                                (movementType == MovementType.YAW ? MovementType.YAW_RIGHT : MovementType.PITCH_UP),
-                        startTimestamp,
-                        endTimestamp)
-                : null;
+        return movements;
     }
 }
